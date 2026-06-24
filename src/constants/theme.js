c24226009ebbadaf2eaafc9968078e25ae86889a
p4n <ibrahimdogan.js@gmail.com>
2026-06-25 00:40:02 +0300

    theme and folder structure

---
+++ src/constants/theme.js
@@ -0,0 +1,13 @@
+export const COLORS = {
+  primary: '#FCA34D',
+  primaryDark: '#F1923A',
+  secondary: '#FE7E3D',
+  accent: '#F19537',
+  foreground: '#1E1F22',
+  bgLight: '#FDE8D1',
+};
+
+export const FONTS = {
+  display: '"Lilita One", sans-serif',
+  sans: '"Quicksand", sans-serif',
+};
